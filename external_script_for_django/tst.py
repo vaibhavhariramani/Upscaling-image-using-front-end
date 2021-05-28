57d8e1b9730a4864dd6498545bb3d35574b81188
VAIBHAV HARIRAMANI <vaibhav.hariramani01@gmail.com>
2021-05-28 17:26:42 +0530

    For Frontend deployment

--- external_script_for_django/tst.py
+++
@@ -1,9 +0,0 @@
-import sys
-import datetime
-
-
-
-output="Hi %s Your file is being processed" % (sys.argv[1])
-out = "HELLO"
-# print(output)
-print(out, sys.argv[1],"WELCOME TO IMAGE UPSCALER YOUR IMAGE IS BEING PROCESSED")
